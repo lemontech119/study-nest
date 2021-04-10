c304e30decd9ac7bd5c33f193b8f3d556ba95f7e
lemontech119 <lemontech119@gmail.com>
2021-04-11 08:26:53 +0900

    Update/NotFoundException

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -39,5 +39,8 @@ export class MoviesController {
         }
     }
 
-
+    @Patch(':id')
+    patch(@Param('id') movieId: string, @Body() updateData) {
+        return this.moviesService.update(movieId, updateData);   
+    }
 }
